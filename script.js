4fbe529061dbdae917439d4e4429cbf919ec0107
ramonsourei <ramonoliveira@staff.sourei.com.br>
2024-03-12 09:05:20 -0300

    second commit

--- script.js
+++ script.js
@@ -2,15 +2,23 @@ function addTask(){
 
     let task = document.createElement("div")
     let buttonRemove = document.createElement("button")
+    let buttonEdit = document.createElement("button")
+    let textButtoEdit = document.createTextNode("Editar")
     let textButtonRemove = document.createTextNode("x")
+
+    buttonEdit.appendChild(textButtoEdit)
     buttonRemove.appendChild(textButtonRemove)
+
     let sectionList = document.querySelector(".section-list")
     let mainSection = document.createElement("div")
+
     mainSection.className = "task-area"
     task.className = "task-section"
+    buttonEdit.className = "edit-task"
     buttonRemove.className = "remove-task"
     sectionList.appendChild(mainSection)
     mainSection.appendChild(task)
+    mainSection.appendChild(buttonEdit)
     mainSection.appendChild(buttonRemove)
     
     let input = document.querySelector(".insert-task").value
@@ -23,6 +31,10 @@ function addTask(){
     buttonRemove.addEventListener("click", () =>{
         removeTask(sectionList, mainSection)
     })
+
+    buttonEdit.addEventListener("click", ()=>{
+        taskEdit(paragrafo,mainSection)
+    })
 }
 
 
@@ -30,6 +42,25 @@ function removeTask(elementDad, elementChild){
 
     elementDad.removeChild(elementChild)
     
+}
+
+function taskEdit(task,section){
+    let buttonConfirmEdit = document.createElement("button")
+    let textButtonConfirm = document.createTextNode("Confirmar")
+    buttonConfirmEdit.appendChild(textButtonConfirm)
+    buttonConfirmEdit.className = "confirm-edit"
+    section.appendChild(buttonConfirmEdit)
+    task.contentEditable = true
+
+    buttonConfirmEdit.addEventListener("click", () =>{
+        confirmEdit(task,section,buttonConfirmEdit)
+    })
+
+}
+
+function confirmEdit(task,section,button){
+    task.contentEditable = false
+    section.removeChild(button)
 }
 
 
